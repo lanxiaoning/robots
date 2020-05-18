d84c2efd754799272f0cec675c5c86de875787c5
lanxiaoning <363309464@qq.com>
2020-05-18 14:05:10 +0800

    Update xxqg.py

--- xxqg.py
+++ xxqg.py
@@ -7,6 +7,9 @@ import datetime
 import pickle
 import os
 import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 __author__ = 'lanxiaoning'
 
@@ -42,6 +45,7 @@ MAX_TRY=7
 RAND_PER=6
 ##cookie内容保存文件定义
 COOKIE_FILE='xuexi.cookie'
+TIMEOUT=30
 
 driver='Application\\chromedriver.exe'
 
@@ -75,7 +79,9 @@ def watch_videos():
         for j in range(1,3):
             titletmp = title_path.replace('rownum', str(i))
             title = titletmp.replace('colnum', str(j))
-            autoclick(title, 0)
+            ###使用WebDriverWait等待来替代自写的等待
+            ##autoclick(title, 0)
+            WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, title))).click()
             windows = browser.window_handles
             browser.switch_to.window(windows[-1])
 
@@ -93,7 +99,9 @@ def watch_videos():
 
             # 获取视频时长
             #video_duration_str = browser.find_element_by_class_name('duration').get_attribute('innerText')
-            video_duration_str=autotextbyclass('duration',0)
+            ##video_duration_str=autotextbyclass('duration',0)
+            ##使用WebDriverWait代替自编写的等待
+            video_duration_str = WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.CLASS_NAME, 'duration'))).get_attribute('innerText')
             ##print('duration:'+video_duration_str)
             video_duration = int(video_duration_str.split(':')[0]) * 60 + int(video_duration_str.split(':')[1])
             print('视频时长:'+str(video_duration)+'秒,等待'+str(video_duration)+'秒')
@@ -122,7 +130,9 @@ def read_articles():
     title_path='//*[@id="root"]/div/div/section/div/div/div/div/div/section/div/div/div/div[1]/div/section/div/div/div/div/div/section/div/div/div/div/div[3]/section/div/div/div/div/div/section/div/div/div[1]/div/div[num]/div/div/div[1]/span'
     for i in range(1,7):
         title=title_path.replace('num',str(i))
-        autoclick(title, 0)
+        ###autoclick(title, 0)
+        ###使用WebDriverWait代替自编写的等待
+        WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, title))).click()
         windows = browser.window_handles
         browser.switch_to.window(windows[-1])
 
@@ -196,13 +206,19 @@ def get_scores():
 
     browser.get(SCORES_LINK)
 
+    '''
+
     time.sleep(2)
 
-    gross_score = browser.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[2]/div[2]/span[1]")\
-
-    get_attribute('innerText')
-
+    gross_score = browser.find_element_by_xpath("//*[@id='app']/div/div[2]/div/div[2]/div[2]/span[1]").get_attribute('innerText')
+    
     today_score = browser.find_element_by_xpath("//span[@class='my-points-points']").get_attribute('innerText')
+    
+    '''
+
+    gross_score=WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, "//*[@id='app']/div/div[2]/div/div[2]/div[2]/span[1]"))).get_attribute('innerText')
+
+    today_score=WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, "//span[@class='my-points-points']"))).get_attribute('innerText')
 
     print("当前总积分：" + str(gross_score))
 
@@ -261,9 +277,11 @@ if __name__ == '__main__':
         print("读完cookie，检测是否已有登陆信息!")
         try:
             browser.get(SCORES_LINK)
-            time.sleep(3)
+            #time.sleep(3)
             ##检查一下我的积分页面有没有退出按钮
-            element=browser.find_element_by_xpath('//*[@id="Ca6ke9htmh6800"]')
+            #element=browser.find_element_by_xpath('//*[@id="Ca6ke9htmh6800"]')
+            ##等待“退出”按钮10秒
+            element = WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="Ca6ke9htmh6800"]')))
         except:
             print('cookie可能已过期，需重新用手机APP扫描登陆')
             #删除cookie文件
